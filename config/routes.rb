5979e448bc4ab950c8d44cdb4a5c4ad88d6cfedf
Hongli Xu <wu402a8zc7@gmail.com>
2019-11-21 23:27:20 -0500

    add dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'dashboard', to: "dashboard#index"
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+  get 'dashboard/index'
   resources :properties
   devise_for :accounts
   root 'public#main'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
