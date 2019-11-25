0a216d9e4ffb94f1e0e9d2d5186bf048a402a5cc
Hongli Xu <wu402a8zc7@gmail.com>
2019-11-25 12:45:15 -0500

    basic property show page

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,11 @@
 module PropertiesHelper
+  def property_thumbnail property
+    img = property.photo.present? ? property.photo.thumb.url : "placeholder.jpg"
+    image_tag img, class: "property-thumb", size: "160"
+  end
+
+  def property_photo_url property
+    property.photo.present? ? property.photo.url : "placeholder.jpg"
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,14 @@
 class Property < ApplicationRecord
   mount_uploader :photo, ImageUploader
   belongs_to :account
+
+  scope :latest, -> { order created_at: :desc }
+
+  def first_name
+    "David"
+  end
+
+  def email
+    "david@gmail.com"
+  end
 end
